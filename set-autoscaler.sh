17e64354e50b77b6d98700c42f5d379c7d6ddd24
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-09-19 10:52:31 +0800

    refinement set-autoscaler.sh
    1) 中间会有个需要输入Y/n的地方，现在自动输入；
    2) add comments

--- set-autoscaler.sh
+++ set-autoscaler.sh
@@ -7,9 +7,10 @@ kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/late
 kubectl patch deployment metrics-server -n kube-system --type 'json' -p '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]'
 
 # apache bench
-apt install apache2-utils
-# 中间会有个回复Y/n的地方，试试如何避免abort（先开始的commit是可以的，后来会abort）
-# 在执行本脚本时，`./set-autoscaler.sh`直接在terminal中回车，不要在它后面同时（一次性）复制其他cmd
+echo -e "Y\n" | apt install apache2-utils # auto answer the "Y/n" field to continue installing process
+
+
+# TEST
 
 # apache bench
 ab -V
@@ -19,6 +20,6 @@ sleep 60 # wait for some time, before patch comes into effect
 kubectl get apiservices |grep metrics
 
 
-# 部署my-scheduler，部署测试用rc
+# EXAMPLES DOCS: 部署my-scheduler，部署测试用rc
 cat usages.txt
 
