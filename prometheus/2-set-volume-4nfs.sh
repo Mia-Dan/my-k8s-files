304117d306a48398185b62928cf3834368a5983f
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-09-26 10:26:40 +0800

    fix nfs setup

--- prometheus/2-set-volume-4nfs.sh
+++ prometheus/2-set-volume-4nfs.sh
@@ -1,32 +1,35 @@
 #! /bin/bash
 
-pwd
+cd /root/my-k8s-files/prometheus
+
+# Install & Setup NFS ---------------------
+
 echo 'Y' | sudo apt install nfs-kernel-server # 1. Install the NFS server on your main system.
+# 挂载文件夹假定命名为 /mnt/nfs/promdata
 sudo mkdir -p /mnt/nfs/promdata # 2. Create a directory to use with Prometheus.
 sudo chown nobody:nogroup /mnt/nfs/promdata 
 sudo chmod 777 /mnt/nfs/promdata 
 
+# to solve issue1: 
+echo '/mnt/nfs/promdata *(insecure,rw,sync,no_root_squash)' >> /etc/exports 
+exportfs -r   #更新配置
+showmount -e
+#    如果生效会显示
+#    Export list for Ubuntu:
+#    /mnt/nfs/promdata *
+/etc/init.d/nfs-kernel-server restart 
+
+# to solve issue2:
+ssh node01 "sudo apt-get install -y nfs-common"
+
+
+# k8s YAML  ---------------------
+
+# edit
 local_ip=$(ip a | sed -rn '/inet (.*) en/p' | cut --delimiter=' ' -f6 | cut --delimiter='/' -f1)
 sed -i "s/<nfs-ip>/$local_ip/" 2-pv/pv-pvc-4nfs.yaml
-
+# apply
 kubectl apply -f 2-pv/pv-pvc-4nfs.yaml
-
-# 1. sudo apt install nfs-kernel-server
-    # ...
-    # Creating config file /etc/idmapd.conf with new version
-    # Adding system user `statd' (UID 115) ...
-    # Adding new user `statd' (UID 115) with group `nogroup' ...
-    # Not creating home directory `/var/lib/nfs'.
-    # Created symlink /etc/systemd/system/multi-user.target.wants/nfs-client.target → /lib/systemd/system/nfs-client.target.
-    # Created symlink /etc/systemd/system/remote-fs.target.wants/nfs-client.target → /lib/systemd/system/nfs-client.target.
-    # nfs-utils.service is a disabled or a static unit, not starting it.
-    # Setting up nfs-kernel-server (1:1.3.4-2.5ubuntu3.5) ...
-    # Created symlink /etc/systemd/system/multi-user.target.wants/nfs-server.service → /lib/systemd/system/nfs-server.service.
-    # Job for nfs-server.service canceled.
-
-    # Creating config file /etc/exports with new version
-
-    # Creating config file /etc/default/nfs-kernel-server with new version
-    # Processing triggers for systemd (245.4-4ubuntu3.18) ...
-    # Processing triggers for man-db (2.9.1-1) ...
-    # Processing triggers for libc-bin (2.31-0ubuntu9.9) ...
+# 应该返回：
+# persistentvolume/pv-nfs-data created
+# persistentvolumeclaim/pvc-nfs-data created
